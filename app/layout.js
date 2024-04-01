658365a5cc436122ab9e4ff02a2de7a53a200cb9
parth_wrappixel <wrappixelTeam.2016@gmail.com>
2024-04-01 10:54:25 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.css'
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false
 
 
 const inter = Inter({ subsets: ["latin"] });
